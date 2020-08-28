5046a68124effbcbd6cdfc6dd98866e471c8058d
ViliusZur <vilius.zurauskas@gmail.com>
2020-08-28 13:56:42 +0300

    added cors and bodyparser

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 // Constants
 const PORT = 8080;
@@ -6,8 +8,12 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+app.use(cors());
+app.use(bodyParser());
+
 app.get('/', (req, res) => {
-  res.send('okay World');
+  res.send('Hello World');
 });
 
 app.listen(PORT, HOST);
