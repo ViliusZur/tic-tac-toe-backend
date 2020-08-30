fc30f8ca3ddb74fe96dde2059495f634ae356c71
ViliusZur <vilius.zurauskas@gmail.com>
2020-08-30 18:59:49 +0300

    improve: deleted uncecessary routes; deleted console.log's; added comments.

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const PORT = 8080;
 // App
 const app = express();
 
+// had to add credentials and origin in cors params, otherwise the cookie would not save in browser
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(bodyParser());
+app.use(bodyParser.json());
 app.use('/api', logs);
 
 app.listen(PORT);
--- routes/logs.js
+++ routes/logs.js
@@ -9,58 +9,35 @@ router.use(session({
   secret: 'sauce',
   saveUninitialized: true,
   resave: true,
-  /*cookie: {
-    secure: false,
-    httpOnly: false,
-    expires: new Date(253402300000000), // expires approximately Friday, 31 Dec 9999 23:59:59 GMT
-  },*/
 }));
 
 let sess; // declare global session variable
 
-router.get('/test', (req, res) => {
-  sess = req.session;
-  if (sess.views) {
-    sess.views += 1;
-  } else {
-    sess.views = 1;
-  }
-  console.log(req.sessionID);
-  res.send(`${sess.views} views`);
-});
-
-router.get('/getSession', (req, res) => {
-  console.log(req.sessionID);
-  res.send(req.session);
-});
-
-router.post('/log', bodyParser(), async (req, res) => {
+router.post('/log', bodyParser.json(), async (req, res) => {
   // log game event
-  console.log(req.sessionID);
   sess = req.session;
-  console.log(sess);
   const [square, value, newGame] = [req.body.square, req.body.value, req.body.newGame];
 
   if (newGame) {
     // if true, then append 'New Game' to log and nullify game squares
-    sess.log.push('New Game');
+    sess.log.unshift('New Game');
     sess.squares = Array(9).fill(null);
     sess.winner = false;
     res.send('new game added to log and squares have been reset');
   } else {
     // else add data to game squares and add transformed data to log
     sess.squares[square] = value;
-    sess.log.push(await logs.transformToReadableString(square, value));
+    sess.log.unshift(await logs.transformToReadableString(square, value));
 
     // check for winner
     const ans = await logs.checkForWinner(sess.squares);
     if (ans !== null) {
-      sess.log.push(`${ans} won the game!`);
+      sess.log.unshift(`${ans} won the game!`);
       sess.winner = ans;
     }
     // check if game finished without a winner
     if (!sess.squares.includes(null) && ans === null) {
-      sess.log.push(`Nobody won the game`);
+      sess.log.unshift('Nobody won the game');
       sess.winner = null;
     }
     res.send('squares and log have been updated');
@@ -71,22 +48,18 @@ router.get('/getLogs', async (req, res) => {
   // send back the session logs array and squares of the game
   sess = req.session;
   if (sess.log === undefined || sess.squares === undefined) {
+    // if the session is new - initiate log, squares, and winner variables
     sess.log = [];
     sess.squares = Array(9).fill(null);
     sess.winner = false;
-    console.log(sess);
-    console.log(req.sessionID);
     res.send({ log: sess.log, squares: sess.squares, winner: sess.winner });
   } else {
-    console.log(sess);
-    console.log(req.sessionID);
     res.send({ log: sess.log, squares: sess.squares, winner: sess.winner });
   }
 });
 
 router.post('/closeSession', async (req, res) => {
   // terminates the session
-  console.log(req.sessionID);
   req.session.destroy();
   res.sendStatus(200);
 });
